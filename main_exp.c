5c5c497aacb4de4feb37fcdd77eb1fe0013b2ece
Julian Cano dev <juliancano.dev@gmail.com>
2020-03-17 12:05:34 -0500

    Bien?

--- main_exp.c
+++ main_exp.c
@@ -4,13 +4,11 @@ int main(void)
 {
 	char c;
     char *s;
-    int n;
 
 	c = 'A';
     s = "camilo";
-    n = 10;
 
-	_printf("Hello %c -- %s --  \n", c, s );
+	_printf("Hello %c -- %s --  \n", c, s);
 
     _printf("percent: %% \n");
 
